54efdb55357d89e623f297579883a0a7cdcbd072
sofrosb <149579577+sofrosb@users.noreply.github.com>
2024-11-18 15:25:57 +0100

    test

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ const App = () => {
   return (
     <>
       <h1 className="flex h-screen w-full items-center justify-center text-3xl text-red-600">
-        SPAM
+        SPAM pizza
       </h1>
     </>
   );

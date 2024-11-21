7c71b1d5554cce358e23d18a4e802ef27cd272b8
Abuckard <alexander.buckard@gmail.com>
2024-11-21 12:07:08 +0100

    Mongo

---
+++ server/connectMongo.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const connectMongo = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_CONNECT_URI)
+        console.log("Connected to MongoDB")
+    } catch (error) {
+        console.log("connect failed" + error.message)
+    }
+    }
+
+module.exports = connectMongo

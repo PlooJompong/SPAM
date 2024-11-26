6f10fa6fde18964098a128be015cb96ea52f4011
sofrosb <149579577+sofrosb@users.noreply.github.com>
2024-11-26 15:54:41 +0100

    landing page, home page, headers, stock status and favicon

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,11 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import User from './models/user.js';
-import Menu from './models/menu.js';
-import Order from './models/orders.js';
-import Stock from './models/stockStatus.js';
-
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import User from "./models/user.js";
+import Menu from "./models/menu.js";
+import Order from "./models/orders.js";
+import Stock from "./models/stockStatus.js";
 
 dotenv.config();
 
@@ -13,15 +13,17 @@ const PORT = process.env.PORT || 8000;
 const MONGO_URI = process.env.MONGO_URI;
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {})
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
+mongoose
+  .connect(process.env.MONGO_URI, {})
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 // Enkel test-API
-app.get('/', async (req, res) => {
-  res.send('Hello, world!');
+app.get("/", async (req, res) => {
+  res.send("Hello, world!");
 });
 
 // Hämta alla objekt
@@ -35,44 +37,44 @@ app.get('/', async (req, res) => {
 // });
 
 // Hämta alla användare
-app.get('/users', async (req, res) => {
+app.get("/users", async (req, res) => {
   try {
     const users = await User.find();
     res.status(200).json(users);
   } catch (err) {
     res.status(500).json(err);
   }
-})
+});
 
 // Hämta alla pizzor
-app.get('/menu', async (req, res) => {
+app.get("/menu", async (req, res) => {
   try {
     const menu = await Menu.find();
     res.status(200).json(menu);
   } catch (err) {
     res.status(500).json(err);
   }
-})
+});
 
 // Hämta orders
-app.get('/order', async (req, res) => {
+app.get("/order", async (req, res) => {
   try {
     const order = await Order.find();
     res.status(200).json(order);
   } catch (err) {
     res.status(500).json(err);
   }
-})
+});
 
 // Lagerstatus
-app.get('/stock', async (req, res) => {
+app.get("/stock", async (req, res) => {
   try {
     const stock = await Stock.find();
     res.status(200).json(stock);
   } catch (err) {
     res.status(500).json(err);
   }
-})
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
